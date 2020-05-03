e8af93b3309b15391e9898288812260da7988e91
tjebo <tjebn@web.de>
2020-05-03 16:30:34 +0100

    interval censoring geom_icens

--- R/geom-km.R
+++ R/geom-km.R
@@ -24,6 +24,66 @@ GeomKm <- ggplot2::ggproto("GeomKm", Geom,
 
 )
 
+
+#' Display Kaplan Meier Curve
+#'
+#' Kaplan Meier Curve
+#'
+#' @export
+#' @rdname geom_icens
+
+
+GeomIcens <- ggplot2::ggproto("GeomIcens", Geom,
+
+                           draw_group = function(data, scales, coordinates, ...) {
+
+                             path <- transform(data, alpha = NA)
+                             GeomPath$draw_panel(path, scales, coordinates)
+
+                           },
+
+                           required_aes = c("x", "y"),
+                           default_aes = ggplot2::aes(colour="black", fill="grey60", size=.75,
+                                                      linetype=1, weight=1, alpha = 1),
+                           draw_key = draw_key_path
+
+
+)
+
+#' Add a Kaplan-Meier survival curve
+#'
+#' @section Aesthetics:
+#' \code{geom_icens} understands the following aesthetics (required aesthetics
+#' are in bold):
+#' \itemize{
+#'   \item \strong{\code{x}} The survival/censoring times. This is automatically mapped by \link{stat_km}
+#'   \item \strong{\code{y}} The survival probability estimates. This is automatically mapped by \link{stat_km}
+#'   smallest level in sort order is assumed to be 0, with a warning
+#'   \item \code{alpha}
+#'   \item \code{color}
+#'   \item \code{linetype}
+#'   \item \code{size}
+#' }
+#'
+#' @inheritParams ggplot2::geom_point
+#' @seealso The default stat for this geom is \code{\link{stat_km}} see
+#'   that documentation for more options to control the underlying statistical transformation.
+#' @export
+#' @examples
+#' sex <- rbinom(250, 1, .5)
+#' df <- data.frame(time = exp(rnorm(250, mean = sex)), status = rbinom(250, 1, .75), sex = sex)
+#' ggplot(df, aes(time = time, status = status, color = factor(sex))) + geom_km()
+
+geom_icens <- function(mapping = NULL, data = NULL, stat = "km",
+                    position = "identity", show.legend = NA,
+                    inherit.aes = TRUE, na.rm = TRUE, ...) {
+  ggplot2::layer(
+    geom = GeomIcens, mapping = mapping, data = data, stat = stat,
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
 #' Add a Kaplan-Meier survival curve
 #'
 #' @section Aesthetics:
--- R/stat-km.R
+++ R/stat-km.R
@@ -12,7 +12,7 @@ StatKm <- ggplot2::ggproto("StatKm", Stat,
                            compute_group = function(data, scales, trans = "identity", firstx = 0, firsty = 1,
                                                     type = "kaplan-meier", start.time = 0) {
 
-                             sf <- survival::survfit.formula(survival::Surv(time = data$time, time2=data$time2, type = "interval2") ~ 1, se.fit = FALSE,
+                             sf <- survival::survfit.formula(survival::Surv(data$time, data$status) ~ 1, se.fit = FALSE,
                                                              type = type, start.time = start.time)
 
                              transloc <- scales::as.trans(trans)$trans
@@ -35,7 +35,48 @@ StatKm <- ggplot2::ggproto("StatKm", Stat,
                            },
 
                            default_aes = ggplot2::aes(y = ..survival.., x = ..time..),
-                           required_aes = c("time")
+                           required_aes = c("time", "status")
+
+
+)
+
+#' @importFrom ggplot2 layer aes ggproto
+#' @import scales
+#' @importFrom survival Surv survfit.formula
+#' @importFrom grid pointsGrob nullGrob unit gpar gList
+
+#' @rdname stat_icens
+#' @export
+
+StatIcens <- ggplot2::ggproto("StatIcens", Stat,
+
+                           compute_group = function(data, scales, trans = "identity", firstx = 0, firsty = 1,
+                                                    type = "kaplan-meier", start.time = 0) {
+
+                             sf <- survival::survfit.formula(survival::Surv(time = data$time, time2 = data$time2) ~ 1, se.fit = FALSE,
+                                                             type = "interval2", start.time = start.time)
+
+                             transloc <- scales::as.trans(trans)$trans
+
+                             if(is.null(sf$surv)) {
+                               x <- rep(sf$time, 2)
+                               sf$surv <- rep(1, length(x))
+                             }
+
+                             x <- c(firstx, sf$time)
+                             y <- transloc(c(firsty, sf$surv))
+                             y[y == -Inf] <- min(y[is.finite(y)])
+                             y[y == Inf] <- max(y[is.finite(y)])
+
+                             step <- dostep(x, y)
+                             df.out <- data.frame(time = step$x, survival = step$y)
+
+                             df.out
+
+                           },
+
+                           default_aes = ggplot2::aes(y = ..survival.., x = ..time..),
+                           required_aes = c("time", "time2")
 
 
 )
@@ -99,6 +140,75 @@ StatKmband <- ggplot2::ggproto("StatKmband", Stat,
 
 
 )
+
+#' Adds a Kaplan Meier Estimate of Survival
+#'
+#' @section Aesthetics:
+#' \code{stat_km} understands the following aesthetics (required aesthetics
+#' are in bold):
+#' \itemize{
+#'   \item \strong{\code{time}} The survival times
+#'   \item \strong{\code{status}} The censoring indicator, see \link[survival]{Surv} for more information.
+#'   \item \code{alpha}
+#'   \item \code{color}
+#'   \item \code{linetype}
+#'   \item \code{size}
+#' }
+#'
+#' @inheritParams ggplot2::stat_identity
+#' @param trans Transformation to apply to the survival probabilities. Defaults
+#'   to "identity". Other options include "event", "cumhaz", "cloglog", or
+#'   define your own using \link{trans_new}.
+#' @param firstx,firsty the starting point for the survival curves. By default,
+#'   the plot program obeys tradition by having the plot start at (0,1).
+#' @param ... Other arguments passed to \code{survival::survfit.formula}
+#' @return a data.frame with additional columns: \item{x}{x in data}
+#'   \item{y}{Kaplan-Meier Survival Estimate at x}
+#' @export
+#'
+#' @rdname stat_km
+#' @details
+#'
+#' This stat is for computing the confidence intervals for the Kaplan-Meier survival estimate for
+#' right-censored data. It requires the aesthetic mapping \code{x} for the
+#' observation times and \code{status} which indicates the event status,
+#' 0=alive, 1=dead or 1/2 (2=death). Logical status is not supported.
+#'
+#'
+#' @examples
+#' sex <- rbinom(250, 1, .5)
+#' df <- data.frame(time = exp(rnorm(250, mean = sex)), status = rbinom(250, 1, .75), sex = sex)
+#' ggplot(df, aes(time = time, status = status, color = factor(sex))) +
+#'  stat_km()
+#'
+#' ## Examples illustrating the options passed to survfit.formula
+#'
+#' p1 <- ggplot(df, aes(time = time, status = status))
+#' p1 + stat_km()
+#' p1 + stat_km(trans = "cumhaz")
+#' # cloglog plots also log transform the time axis
+#' p1 + stat_km(trans = "cloglog") + scale_x_log10()
+#' p1 + stat_km(type = "fleming-harrington")
+#' p1 + stat_km(start.time = 5)
+#'
+
+stat_icens <- function(mapping = NULL, data = NULL, geom = "icens",
+                    position = "identity", show.legend = NA, inherit.aes = TRUE,
+                    se = TRUE, trans = "identity", firstx = 0, firsty = 1,
+                    type = "kaplan-meier", start.time = 0) {
+  ggplot2::layer(
+    stat = StatIcens,
+    data = data,
+    mapping = mapping,
+    geom = geom,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(trans = trans, firstx = firstx, firsty = firsty,
+                  type = type, start.time = start.time)
+  )
+
+}
 
 
 #' Adds a Kaplan Meier Estimate of Survival
